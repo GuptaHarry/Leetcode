fc5d510cc326a4382cb6c3f5add4c7197e37468b
GuptaHarry <136607363+GuptaHarry@users.noreply.github.com>
2025-07-14 09:22:04 +0530

    0198-HOUSE-ROBBER Stats: Time: 0 ms (100%), Space: 10.7 MB (22.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
-int find(int index, vector<int>&dp , vector<int>nums , int n ){
-      if(index >=n)
-      return 0;
-
-      if(dp[index]!=-1)
-      return dp[index];
-
-      dp[index]= max ( nums[index]+ find(index+2 , dp , nums,n) ,
-      find(index+1,dp,nums,n));
-      return dp[index];
-}
     int rob(vector<int>& nums) {
 
-     // top down appraoch 
-     // variable changing -> index only => 1D dp required 
-     // max index reachable ->   size +2 
-     vector<int>dp(nums.size()+2 ,-1);
+     // bottom up appraoch 
+     vector<int>dp(nums.size()+2,-1);
      dp[nums.size()+1] = dp[nums.size()]=0;
-
-     return find(0, dp, nums,nums.size());
+     for(int i = nums.size()-1 ; i>=0 ;i--){
+        dp[i]= max(nums[i]+dp[i+2] , dp[i+1]);
+     }
+     return dp[0];
     }
 };
