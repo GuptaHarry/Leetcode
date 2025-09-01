41ee7d634173a93e0bf144c0415b519bc82a995c
GuptaHarry <136607363+GuptaHarry@users.noreply.github.com>
2025-09-01 20:26:15 +0530

    3600-FIND-THE-K-TH-CHARACTER-IN-STRING-GAME-I Stats: Time: 4 ms (21.41%), Space: 10.8 MB (6.58%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    char kthCharacter(int k) {
-        
-        string ans = "a";
-        while(ans.size()<k){
-            string temp="";
-            for(int i=0;i<ans.size();i++){
-                int c = ans[i];
-                if(c==122)
-                c=97;
-                else
-                c++;
-                char d = c;
-                temp+=d;
-            }
-            ans+=temp;
+    void find(string &ans, int k) { // pass by reference
+        // base case
+        if (ans.size() >= k)
+            return;
+
+        // build next half
+        string temp = "";
+        for (int i = 0; i < ans.size(); i++) {
+            int c = ans[i];
+            c == 'z' ? c = 'a' : c++;
+            temp += (char)c;
         }
-        return ans[k-1];
+
+        ans += temp;
+
+        // recursive call
+        find(ans, k);
+    }
+
+    char kthCharacter(int k) {
+        string ans = "a";
+        find(ans, k);
+        return ans[k-1];  // indexing fix
     }
 };
