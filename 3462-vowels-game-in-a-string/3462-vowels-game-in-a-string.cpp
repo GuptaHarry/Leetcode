002e1f1dcd6a691fdd70c37b5c2cce336c462637
GuptaHarry <136607363+GuptaHarry@users.noreply.github.com>
2025-09-12 09:35:14 +0530

    3462-VOWELS-GAME-IN-A-STRING Stats: Time: 4 ms (78.54%), Space: 19.2 MB (25.75%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -1,69 +1,8 @@
 class Solution {
 public:
-  int find(string temp , bool turn){
-
-     if(!turn){
-        // alice 
-        int maxOdd=-1;
-        int count=0;
-        for(int i=0;i<temp.size();i++){
-            if(temp[i]=='a'|| temp[i]=='e'|| temp[i]=='i' || temp[i]=='o'||temp[i]=='u'){
-                count++;
-                if(count%2!=0)
-                maxOdd=i;
-
-            }
-        }
-
-        return maxOdd;
-     }
-     else{
-        int maxEven=-1;
-        int count=0;
-        for(int i=0;i<temp.size();i++){
-            if(temp[i]=='a'|| temp[i]=='e'|| temp[i]=='i' || temp[i]=='o'||temp[i]=='u'){
-                count++;
-                if(count%2==0)
-                maxEven=i;
-
-            }
-        }
-
-        return maxEven;
-     }
-  }
     bool doesAliceWin(string s) {
-        
-
-        bool turn = false; // for alice true for bob
-        string temp = s;
-
-        while( temp.size()!=0){
-
-            if(!turn){
-                // alice turn -> odd length substring
-                int odd_index = find(temp,turn);
-                if(odd_index==-1)
-                return false;
-                string res="";
-                for(int i=odd_index+1;i<temp.size();i++)
-                 res+=temp[i];
-
-                 temp = res;
-                 turn = !turn;
-            }else{
-                int even_index = find(temp,turn);
-                    string res="";
-                    if(even_index==-1)
-                    return true;
-                    for(int i=even_index+1;i<temp.size();i++)
-                    res+=temp[i];
-
-                    temp = res;
-                    turn = !turn;
-                }
-            }
-        
-        return true;
+        return ranges::any_of(s, [](char c) {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        });
     }
 };
