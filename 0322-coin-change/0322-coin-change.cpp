c151e48ddafa19dc88a94b85f9b0da286fe00d34
GuptaHarry <136607363+GuptaHarry@users.noreply.github.com>
2025-09-17 07:41:45 +0530

    0322-COIN-CHANGE Stats: Time: 63 ms (11.62%), Space: 52.8 MB (17.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-
-  int find(int index , int amount ,vector<int>& coins , vector<vector<int>>&dp ){
-
-      if(amount==0)
-      return 0;
-
-      if(index==coins.size())
-      return INT_MAX;  // no coins left
-
-      if(dp[index][amount]!=-1)
-      return  dp[index][amount];
-
-
-      int notTake = find(index+1,amount,coins,dp);
-
-
-      int take = INT_MAX;
-      if(coins[index]<=amount)
-      take = find(index,amount-coins[index],coins,dp);
-      if(take != INT_MAX)
-      take+=1;
-   return dp[index][amount] = min(notTake , take);
-        
-  }
     int coinChange(vector<int>& coins, int amount) {
-        
-        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
-        int ans = find(0,amount,coins,dp);
-        // return dp[coins.size()][amount];
-          return (ans == INT_MAX ? -1 : ans);
+        int n = coins.size();
+        const int INF = 1e9; // use large number instead of INT_MAX
+
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INF));
+
+        // Base case: amount = 0 requires 0 coins
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+
+        // Fill DP table
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 1; j <= amount; j++) {
+                int notTake = dp[i + 1][j]; // skip coin i
+                int take = INF;
+                if (j >= coins[i] && dp[i][j - coins[i]] != INF) {
+                    take = 1 + dp[i][j - coins[i]]; // take coin i
+                }
+                dp[i][j] = min(notTake, take);
+            }
+        }
+
+        return dp[0][amount] == INF ? -1 : dp[0][amount];
     }
 };
