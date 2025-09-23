4d367ee6628eee6d106e024905d6091af2112ff1
GuptaHarry <136607363+GuptaHarry@users.noreply.github.com>
2025-09-23 09:16:13 +0530

    0165-COMPARE-VERSION-NUMBERS Stats: Time: 0 ms (100%), Space: 8.1 MB (87.43%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,99 +1,35 @@
 class Solution {
 public:
-    int compareVersion(string version1, string version2) {
-        
-        vector<int>s1;
-        vector<int>s2;
-
-        version1+='.';
-        version2+='.';
-        int i =0;
-        int i1=0;
-        while(i1<version1.size()){
-
-            if(version1[i1]!='.')
-            i1++;
-            else{
-                s1.push_back(stoi(version1.substr(i,i1-i)));
-                i1++;
-                i=i1;
-            }
-        }
- int j =0;
-        int j1=0;
-        while(j1<version2.size()){
-
-            if(version2[j1]!='.')
-            j1++;
-            else{
-                s2.push_back(stoi(version2.substr(j,j1-j)));
-                j1++;
-                j=j1;
-            }
-        }
+    int compareVersion(string v1, string v2) {
 
 
-        i1=0;
-        j1 =0;
+     // previois wqas O(n) timme and spcae 
+     /// mroe optimsid 
 
-        while(i1<s1.size() && j1<s2.size()){
-                
-                if(s1[i1]<s2[j1])
-                return -1;
-                else if ( s1[i1]>s2[j1])
-                return 1;
-                else
-                {
-                   i1++;
-                   j1++;
-                }
-        }
-         // check versions 
-        
-         if(i1==s1.size() && j1==s2.size())
-         return 0;
-         else
-         {
-             if(s2.size()>s1.size()){
-                 int k = s2.size()-s1.size();
-                 while(k--)
-                 s1.push_back(0);
+     // find the whole number
 
+     int n1 = v1.size(), n2 = v2.size();
+     int x1=0,x2=0;
+      
+       for(int i=0,j=0;i<n1||j<n2;i++,j++){
+          
+          while(i<n1 && v1[i]!='.'){
+            x1 = 10*x1+(v1[i++]-'0');
+          }
 
-                 while(i1<s1.size() && j1<s2.size()){
-                    if(s1[i1]<s2[j1])
-                    return -1;
-                    else if (s1[i1]>s2[j1])
-                    return 1;
-                    else{
-                        i1++;
-                        j1++;
-                    }
-                 }
-                 return 0;
-             }
-             else{
-                if(s1.size()>s2.size()){
-                 int k = s1.size()-s2.size();
-                 while(k--)
-                 s2.push_back(0);
+          while(j<n2 && v2[j]!='.'){
+            x2 = 10*x2 + (v2[j++]-'0');
+          }
 
+          if(x1<x2)
+          return -1;
+          else if(x1>x2)
+          return 1;
+          
+          x1=0;
+          x2=0;
+       }
+       return 0;
 
-                 while(i1<s1.size() && j1<s2.size()){
-                    if(s1[i1]<s2[j1])
-                    return -1;
-                    else if (s1[i1]>s2[j1])
-                    return 1;
-                    else{
-                        i1++;
-                        j1++;
-                    }
-                 }
-                 return 0;
-             }
-         }
-         }
-
- return 0;
     }
 };
