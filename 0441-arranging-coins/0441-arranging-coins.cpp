51c4aa5849797ac25964e7fcc7c67af71ab98529
GuptaHarry <136607363+GuptaHarry@users.noreply.github.com>
2025-11-10 20:02:17 +0530

    0441-ARRANGING-COINS Stats: Time: 0 ms (100%), Space: 8.9 MB (3.6%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int arrangeCoins(int n) {
         
-        int val = n;
 
- if(n==1)
- return 1;
+        int s = 1,e=n;
         int ans=0;
-        for(int i=1;i<n;i++){
-          
-          if(val<i)
-          break;
-          val-=i;
-          ans++;
+        while(s<=e){
 
+            int mid = s+(e-s)/2;
+
+            if(mid <=2LL*n/(mid+1)){
+            ans = mid;
+            s = mid+1;
+            }
+            else
+            e=mid-1;
         }
         return ans;
     }
