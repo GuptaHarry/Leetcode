46eeb97f3316593ab8f015b088c1474e4c7460f8
GuptaHarry <136607363+GuptaHarry@users.noreply.github.com>
2025-11-10 11:16:22 +0530

    0268-MISSING-NUMBER Stats: Time: 0 ms (100%), Space: 22 MB (13.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
 
-     int n = nums.size();
-      unordered_map<int,int>m;
-      for(int i=0;i<nums.size();i++) 
-       {
-         m[nums[i]]=i;
-       }
+        int s=0,e=nums.size()-1;
+        int ans =0;
+        while(s<=e){
 
-       for(int i=0;i<=nums.size();i++)
-       if(m.find(i)==m.end())
-       return i;
-
-       return -1;
+            int mid = s+(e-s)/2;
+            if(mid==nums[mid]){
+                // pattenr not broke 
+                s=mid+1;
+            }
+            else if (mid!=nums[mid]){
+                ans = mid;
+                e = mid-1;
+            }
+        }
+        // last case 
+        if(nums.size()-1 == nums[nums.size()-1])
+        return nums.size();
+        // return -1;
+        else
+        return ans;
     }
 };
