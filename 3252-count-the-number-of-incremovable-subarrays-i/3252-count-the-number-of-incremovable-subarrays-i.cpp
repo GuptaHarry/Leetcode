3ed1ea51c808e26822d702b0415859b6272f31db
GuptaHarry <136607363+GuptaHarry@users.noreply.github.com>
2025-11-11 12:33:28 +0530

    3252-COUNT-THE-NUMBER-OF-INCREMOVABLE-SUBARRAYS-I Stats: Time: 0 ms (100%), Space: 22.8 MB (25.75%) - LeetHub

---
+++ 3252-count-the-number-of-incremovable-subarrays-i/3252-count-the-number-of-incremovable-subarrays-i.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int incremovableSubarrayCount(vector<int>& nums) {
+        
+        int ans =0;
+        vector<int>pre,suf;
+        for(int i=0;i<nums.size();i++){
+            if(pre.empty() || nums[pre.back()]<nums[i])
+            pre.push_back(i);
+            else
+            break;
+        }
+
+         for(int i=nums.size()-1;i>=0;i--){
+            if(suf.empty() || nums[suf.back()]>nums[i])
+            suf.push_back(i);
+            else
+            break;
+        }
+
+        // check for overlap 
+        if(pre.size()+suf.size()>nums.size())
+        return 1LL * nums.size() *(nums.size()+1)/2;
+ans = pre.size()+suf.size();
+
+        reverse(suf.begin(),suf.end());
+        int i=0;
+        int j=0;
+        while(i<pre.size() && j<suf.size()){
+            if(nums[pre[i]]<nums[suf[j]]){
+                ans += (suf.size()-j);
+                i++;
+            }
+            else
+            j++;
+        }
+        return ans+1;
+       
+    }
+};
