0f08d92da9c23e779cb4093536bea886fdef6c9e
GuptaHarry <136607363+GuptaHarry@users.noreply.github.com>
2025-11-27 08:33:51 +0530

    0653-TWO-SUM-IV-INPUT-IS-A-BST Stats: Time: 6 ms (33.47%), Space: 38.8 MB (34.78%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,42 +1,11 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
+    unordered_set<int> set;
+
 public:
-void inorder(TreeNode * root , vector<int>&in){
-    if(!root)
-    return;
-
-    inorder(root->left,in);
-    in.push_back(root->val);
-    inorder(root->right,in);
-}
     bool findTarget(TreeNode* root, int k) {
-        
-        vector<int>in;
-        inorder(root,in);
-
-        // sorted arry 
-        int s =0 , e = in.size()-1;
-        while(s<e){
-            if(in[s]+in[e]==k)
-            return true;
-
-            if(in[s]+in[e]<k)
-            s++;
-            else
-            e--;
-        }
-
-        return false;
-
+        if (!root) return false;
+        if (set.count(k - root->val)) return true;
+        set.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
     }
 };
